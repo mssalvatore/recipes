e05e0206001514ea483ffbd9b0910edeac296e91
Mike Salvatore <mike.s.salvatore@gmail.com>
2024-07-18 21:24:55 -0400

    Add fix-quotes.sh

---
+++ fix-quotes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# A utility for replacing non-ASCII quotes with ASCII quotes.
+
+find recipes/ -type f -name '*.md' -exec sed -E -i "s/’/'/g" {} \;
+find recipes/ -type f -name '*.md' -exec sed -E -i 's/”/"/g' {} \;
+find recipes/ -type f -name '*.md' -exec sed -E -i 's/“/"/g' {} \;
