55d185fb99da25603cc25b74d206f4a1100478cc
Mike Salvatore <mike.s.salvatore@gmail.com>
2024-07-19 21:03:34 -0400

    Add inverted ' to fix-quotes.sh

--- fix-quotes.sh
+++ fix-quotes.sh
@@ -3,5 +3,6 @@
 # A utility for replacing non-ASCII quotes with ASCII quotes.
 
 find recipes/ -type f -name '*.md' -exec sed -E -i "s/’/'/g" {} \;
+find recipes/ -type f -name '*.md' -exec sed -E -i "s/‘/'/g" {} \;
 find recipes/ -type f -name '*.md' -exec sed -E -i 's/”/"/g' {} \;
 find recipes/ -type f -name '*.md' -exec sed -E -i 's/“/"/g' {} \;
