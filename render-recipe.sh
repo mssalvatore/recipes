9fdd6707c60a80b1ad5e8fcb3932d13704cf49cc
Mike Salvatore <mike.s.salvatore@gmail.com>
2024-07-19 22:05:40 -0400

    Add CSS for print media

--- render-recipe.sh
+++ render-recipe.sh
@@ -6,6 +6,7 @@
 pandoc \
     --standalone \
     --css=static/recipe.css \
+    --css=static/print.css \
     --include-after-body=static/recipe.js \
     --mathjax=https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js \
     --from=markdown \
